7895c7a849d006a3c3f5eb6ca65ccde204f51166
Jean Carlo <jeancarlomo@hotmail.com>
2021-04-07 11:09:02 -0300

    Cartas girando

--- script.js
+++ script.js
@@ -1,13 +1,67 @@
-let contador = 0;
 
+
+/*--------------------------perguntando o numerode cartas*/
+let contador = 0;
+let numberOfCards;
 do{
-     
-    let numberOfCards = prompt('Com quantas cartas você quer jogar?')
+      numberOfCards = prompt('Com quantas cartas você quer jogar?')
     if(numberOfCards%2!==0 || numberOfCards<4 || numberOfCards>14){
-        alert(`Número de cartas inválido. Por favor selecione um número1 par entre 4 e 14`)
+        alert(`Número de cartas inválido. Por favor selecione um número par entre 4 e 14`)
     }else{
         break;
-    }1
+    }
     contador++
     console.log(`Rodei ${contador}`)
-}while(true)
+}while(true)  
+
+/*-------checking number of cards--
+ console.log(numberOfCards)
+ console.log(typeof(numberOfCards))*/
+ /*---*/
+
+ /*---------------adicionando as cartas
+ const lista = document.querySelector('.card-list')
+numberOfCards= parseInt(numberOfCards)
+console.log(typeof(numberOfCards))
+
+for(let i=0; i<numberOfCards;i++){
+
+    lista.innerHTML += ` <li class="card"> <img src="images/front.png"/</li>`;
+    
+    console.log(i)
+}*/
+
+
+
+const lista = document.querySelector('.card-list')
+numberOfCards= parseInt(numberOfCards)
+console.log(typeof(numberOfCards))
+
+for(let i=0; i<numberOfCards;i++){
+
+    lista.innerHTML += `
+    <li onclick="turnCard('.n${i}')" class="turn-anchor n${i}">
+        <div class="card"  > <img src="images/front.png"> </div>
+        <div class="card back-face"> <img src="images/fiestaparrot.gif"></div> 
+    </li> `
+    
+    console.log(i)
+    console.log(`n${i}`)
+
+
+}
+
+
+
+
+
+function turnCard(cardIndex){
+    
+    console.log(cardIndex)
+    const frontFace = document.querySelector('.card-list ' +cardIndex + ' .card')
+    frontFace.classList.add('turn')
+    
+    const backFace = document.querySelector('.card-list ' +cardIndex +' .back-face')
+    backFace.classList.add('turn-back-face')
+}
+
