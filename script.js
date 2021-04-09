c6ac5bad86acacc300f9808095d0c0aabd360ac7
Jean Carlo <jeancarlomo@hotmail.com>
2021-04-09 18:00:43 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -1,127 +1,123 @@
-
-
-/*--------------------------Asking number of cards*/
-let contador = 0;
 let numberOfCards;
+let images = []
+let chosenCards = [];
+let flipped =false;
+let firstCard,secondCard,firstParent,secondParent;
+let attempts=0;
+let stay=false;
+let counter=0;
 
-do{
-      numberOfCards = prompt('Com quantas cartas você quer jogar?')
-    if(numberOfCards%2!==0 || numberOfCards<4 || numberOfCards>14){
-        
-        alert(`Número de cartas inválido. Por favor selecione um número par entre 4 e 14`)
-    }else{
-        
-        break;
-    }
-}while(true)  
+AskNumberOfCards()
 
-/*-----------------------------------Creating the arrays--*/
+function AskNumberOfCards(){
 
-let images =  ["images/bobrossparrot.gif","images/explodyparrot.gif","images/fiestaparrot.gif","images/metalparrot.gif","images/revertitparrot.gif","images/tripletsparrot.gif","images/unicornparrot.gif"];
-let chosenCards =[];
-let x = (numberOfCards/2)
-numberOfCards= parseInt(numberOfCards)
-/*--------------------------------------Deixando a array aleatoria e salvando na array de cartas escolhidas*/
+
+    do{
+        numberOfCards = prompt('Com quantas cartas você quer jogar?')
+      if(numberOfCards%2!==0 || numberOfCards<4 || numberOfCards>14){
+          
+          alert(`Número de cartas inválido. Por favor selecione um número par entre 4 e 14`)
+      }else{
+          
+          break;
+      }
+  }while(true)  
+
+  createImageArrays()
+}
+
+
+
+function createImageArrays(){
+    images =  ["images/bobrossparrot.gif","images/explodyparrot.gif","images/fiestaparrot.gif","images/metalparrot.gif","images/revertitparrot.gif","images/tripletsparrot.gif","images/unicornparrot.gif"];
+  
     images.sort(comparador);
     function comparador(){
         return Math.random() - 0.5;
     }
     
-    for(let i = 0;i<x;i++){
-    chosenCards.push([images[i]])
+    for(i=0;i<numberOfCards/2;i++){
+        chosenCards.push(images[i])
     }
-/*------------------------------------------Randomizing pairs in HTML-----------------*/
-
-const list = document.querySelector('.card-list');
-
-for(let i=0; i<x;i++){
-
-    list.innerHTML += `
-    <li onclick="turnCard('.n${i}')" class="turn-anchor n${i}">
-        <div class="card" > <img src="images/front.png" > </div>
-        <div class="card back-face"> <img src=${chosenCards[i]}></div> 
-    </li> `
-}
-    chosenCards.sort(comparador);
-
-
-    function comparador(){
-    return Math.random() - 0.5;
-}
     
-    for(let i=0; i<x;i++){
-    list.innerHTML += `
-    <li onclick="turnCard('.n${i+x}',this)" class="turn-anchor n${i+x}">
-        <div class="card" > <img src="images/front.png" > </div>
-        <div class="card back-face"> <img src=${chosenCards[i]}></div> 
-    </li> `
-    }   
-
-/*--------------------------------------turning cards------------------*/
-let flipped =false;
-let firstCard,firstCardBack,secondCard,secondCardBack,parent;
-let src1,src2;
-let attempts=0;
-
-function turnCard(cardIndex,clickedCard){
-    attempts++
-    clickedCard = document.querySelector('.card-list ' +cardIndex)
-    const frontFace = document.querySelector('.card-list ' +cardIndex + ' .card')
-    frontFace.classList.add('turn')
-    const backFace = document.querySelector('.card-list ' +cardIndex +' .back-face')
-    backFace.classList.add('turn-back-face')
-    compare(frontFace,backFace,clickedCard)  
+    printCards()
 }
 
-function compare(frontFace,backFace,clickedCard){
-   
-     if(!flipped){
-         
+function printCards(){
+
+    const ul = document.querySelector('.card-list');
+    
+    for(let j=0;j<2;j++){
+        for(let i=0; i<numberOfCards/2;i++){
+            ul.innerHTML += `
+                <li onclick="turnCard(this)" class="turn-anchor" id=${chosenCards[i]}>
+                <div class="card front-face" > <img src="images/front.png" > </div>
+                <div class="card back-face"> <img src=${chosenCards[i]}></div> 
+                </li> `
+        }
+
+            if(j===1){
+                break;
+            }
+                
+                chosenCards.sort(comparador);
+                function comparador(){
+                    return Math.random() - 0.5;
+                }
+           
+    }
+
+}
+
+function turnCard(clickedCard){
+    attempts++
+    if(stay) return;
+    
+    clickedCard.classList.add('turn')
+    id=clickedCard.id
+    
+   if(!flipped){
         flipped=true;
-        firstCard=frontFace
-        parent=firstCard.parentNode
-        firstCardBack=backFace
-        src1=firstCardBack.querySelector('img').src;
+        firstCard=id
+        firstParent=clickedCard;
         
     }else{
-        flipped = false;
-        secondCard=frontFace
-        secondCardBack=backFace
-        src2=secondCardBack.querySelector('img').src;
-        compareMatch()
-       
+        flipped=false;
+        secondCard=id
+        secondParent=clickedCard;
+        checkMatch()
     }
-
-    if(contador===x){
+    if(counter===numberOfCards/2){
         setTimeout(() => {
             alert(`Você ganhou em ${attempts} rodadas`);
         }, 500);
     }
 }
 
+function checkMatch(){
 
-function compareMatch(){
-    if(src1===src2){
-        removeOnclick()
+    if(firstCard===secondCard){
         
+        removeOnclick()
     }else{
-        flipBack()
-     }
+
+        turnBack(firstParent,secondParent)
+    }
 }
+
 
 function removeOnclick(){
-    firstCard.parentNode.removeAttribute("onclick");
-        secondCard.parentNode.removeAttribute("onclick");
-        contador++
+    firstParent.removeAttribute('onclick')
+    secondParent.removeAttribute('onclick')
+    counter++
 }
 
-function flipBack(){
-   
-    setTimeout( () =>{
-        firstCard.classList.remove('turn');
-        firstCardBack.classList.remove('turn-back-face');
-        secondCard.classList.remove('turn');
-        secondCardBack.classList.remove('turn-back-face');
+function turnBack(){
+    stay = true;
+    setTimeout(() => {
+        firstParent.classList.remove('turn')
+        secondParent.classList.remove('turn')
     
-    },1000);
+        stay=false
+    }, 1000);
 }
